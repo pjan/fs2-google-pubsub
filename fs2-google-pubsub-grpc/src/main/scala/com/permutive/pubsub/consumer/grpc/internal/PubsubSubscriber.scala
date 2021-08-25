5fc37cbd80493393b7da44a4199b7e98d623c78c
Ben Stewart <benalanstewart@gmail.com>
2021-08-25 14:28:59 +0100

    wip

--- fs2-google-pubsub-grpc/src/main/scala/com/permutive/pubsub/consumer/grpc/internal/PubsubSubscriber.scala
+++ fs2-google-pubsub-grpc/src/main/scala/com/permutive/pubsub/consumer/grpc/internal/PubsubSubscriber.scala
@@ -1,7 +1,7 @@
 package com.permutive.pubsub.consumer.grpc.internal
 
-import cats.Applicative
-import cats.effect.kernel.{Resource, Sync}
+import cats.effect.kernel.{Async, Resource, Sync}
+import cats.effect.std.{Dispatcher, Queue, QueueSink}
 import cats.syntax.all._
 import com.google.api.core.ApiService
 import com.google.api.gax.batching.FlowControlSettings
@@ -14,19 +14,20 @@ import com.permutive.pubsub.consumer.{Model => PublicModel}
 import fs2.Stream
 import org.threeten.bp.Duration
 
-import java.util.concurrent.{BlockingQueue, LinkedBlockingQueue, TimeUnit}
+import java.util.concurrent.TimeUnit
 
 private[consumer] object PubsubSubscriber {
 
-  def createSubscriber[F[_]: Sync](
+  private def createSubscriber[F[_]: Sync](
     projectId: PublicModel.ProjectId,
     subscription: PublicModel.Subscription,
     config: PubsubGoogleConsumerConfig[F],
-    queue: BlockingQueue[Either[InternalPubSubError, Model.Record[F]]],
+    queue: QueueSink[F, Either[InternalPubSubError, Model.Record[F]]],
+    dispatcher: Dispatcher[F],
   ): Resource[F, ApiService] =
     Resource.make(
       Sync[F].delay {
-        val receiver         = new PubsubMessageReceiver(queue)
+        val receiver         = new PubsubMessageReceiver(queue, dispatcher)
         val subscriptionName = ProjectSubscriptionName.of(projectId.value, subscription.value)
 
         // build subscriber with "normal" settings
@@ -49,43 +50,47 @@ private[consumer] object PubsubSubscriber {
             .getOrElse(builder)
             .build()
 
-        sub.addListener(new PubsubErrorListener(queue), MoreExecutors.directExecutor)
+        sub.addListener(new PubsubErrorListener(queue, dispatcher), MoreExecutors.directExecutor)
 
         sub.startAsync()
       }
     )(service =>
       Sync[F]
-        .blocking(service.stopAsync().awaitTerminated(config.awaitTerminatePeriod.toSeconds, TimeUnit.SECONDS))
+        .blocking(
+          service.stopAsync().awaitTerminated(config.awaitTerminatePeriod.toSeconds, TimeUnit.SECONDS)
+        )
         .handleErrorWith(config.onFailedTerminate)
     )
 
-  class PubsubMessageReceiver[F[_]: Sync, L](queue: BlockingQueue[Either[L, Model.Record[F]]]) extends MessageReceiver {
+  class PubsubMessageReceiver[F[_]: Sync, L](
+    queue: QueueSink[F, Either[L, Model.Record[F]]],
+    dispatcher: Dispatcher[F],
+  ) extends MessageReceiver {
     override def receiveMessage(message: PubsubMessage, consumer: AckReplyConsumer): Unit =
-      queue.put(Right(Model.Record(message, Sync[F].delay(consumer.ack()), Sync[F].delay(consumer.nack()))))
+      dispatcher.unsafeRunSync(
+        queue.offer(Right(Model.Record(message, Sync[F].delay(consumer.ack()), Sync[F].delay(consumer.nack()))))
+      )
   }
 
-  class PubsubErrorListener[R](queue: BlockingQueue[Either[InternalPubSubError, R]]) extends ApiService.Listener {
+  class PubsubErrorListener[F[_], R](
+    queue: QueueSink[F, Either[InternalPubSubError, R]],
+    dispatcher: Dispatcher[F],
+  ) extends ApiService.Listener {
     override def failed(from: ApiService.State, failure: Throwable): Unit =
-      queue.put(Left(InternalPubSubError(failure)))
+      dispatcher.unsafeRunSync(queue.offer(Left(InternalPubSubError(failure))))
+
   }
 
-  def takeNextElement[F[_]: Sync, A](messages: BlockingQueue[A]): F[A] =
-    for {
-      nextOpt <- Sync[F].delay(Option(messages.poll()))
-      next    <- nextOpt.fold(Sync[F].blocking(messages.take()))(Applicative[F].pure)
-    } yield next
-
-  def subscribe[F[_]: Sync](
+  def subscribe[F[_]: Async](
     projectId: PublicModel.ProjectId,
     subscription: PublicModel.Subscription,
     config: PubsubGoogleConsumerConfig[F],
   ): Stream[F, Model.Record[F]] =
     for {
-      queue <- Stream.eval(
-        Sync[F].delay(new LinkedBlockingQueue[Either[InternalPubSubError, Model.Record[F]]](config.maxQueueSize))
-      )
-      _    <- Stream.resource(PubsubSubscriber.createSubscriber(projectId, subscription, config, queue))
-      next <- Stream.repeatEval(takeNextElement(queue))
-      msg  <- Stream.fromEither[F](next)
+      queue      <- Stream.eval(Queue.bounded[F, Either[InternalPubSubError, Model.Record[F]]](config.maxQueueSize))
+      dispatcher <- Stream.resource(Dispatcher[F])
+      _          <- Stream.resource(PubsubSubscriber.createSubscriber(projectId, subscription, config, queue, dispatcher))
+      next       <- Stream.fromQueueUnterminated(queue)
+      msg        <- Stream.fromEither[F](next)
     } yield msg
 }
